c4433881f1db8f5043138012f86c11db3fc9190a
ArondevIta <aronamaral@gmail.com>
2020-06-18 20:25:01 -0300

    mobile and fixes

--- backend/src/controllers/ItemsController.ts
+++ backend/src/controllers/ItemsController.ts
@@ -1,15 +1,17 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"
 import knex from '../database/connection'
+import ip from 'ip'
 
 class ItemsController {
   async index(req: Request, res: Response) {
+    const myip = ip.address()
     const items = await knex('items').select('*')
-  
+    
     const serializedItems = items.map(item => {
       return {
         id: item.id,
         title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`
+        image_url: `http://${myip}:3333/uploads/${item.image}`
       }
     })
   
--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -1,5 +1,5 @@
-import { Request, Response} from 'express'
 import knex from '../database/connection'
+import { Request, Response} from 'express'
 
 class PointsController {
 
